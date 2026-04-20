b21e63ba1c7f28c90e1026dcb937f0359c48faa4
cattrang69 <148383267+cattrang69@users.noreply.github.com>
2026-04-19 22:19:10 -0400

    final update

--- logout.php
+++ logout.php
@@ -3,15 +3,11 @@
 // Retrieved 2026-04-16, License - CC BY-SA 2.5
 
 <?php
-// Initialize the session.
-// If you are using session_name("something"), don't forget it now!
+
 session_start();
 
-// Unset all of the session variables.
 $_SESSION = array();
 
-// If it's desired to kill the session, also delete the session cookie.
-// Note: This will destroy the session, and not just the session data!
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -20,7 +16,6 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// Finally, destroy the session.
 session_destroy();
 header("Location: index.php");
 exit();
